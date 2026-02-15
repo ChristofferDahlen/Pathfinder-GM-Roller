2be6211fc910c8a1183e470b7f03785d60154773
Christoffer Dahlen <74780529+ChristofferDahlen@users.noreply.github.com>
2026-02-15 23:57:31 +0100

    Adjust layout for rollMins input in DCPeriod.vue
    Wrapped the rollMins InputNumber component in a styled div to ensure consistent spacing and alignment. This improves the overall layout and visual clarity of the input fields in the component.

--- app/pane/parts/DCPeriod.vue
+++ app/pane/parts/DCPeriod.vue
@@ -81,7 +81,7 @@ function restart() {
   <div>
     <div class="inline-block mx-2"><Checkbox v-model="isRolling" binary class="" @change="change"/></div>
     <div class="inline-block">Roll every</div>
-    <InputNumber v-model="rollMins" fluid/>
+    <div class="inline-block mx-1 w-12" ><InputNumber v-model="rollMins" fluid/></div>
     <div class="inline-block">min and</div>
     <div class="mx-1 w-12 inline-block"><InputNumber v-model="rollSec" :min="0" :max="60" fluid/></div>seconds
   </div>
