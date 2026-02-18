8cea3612d1c2132dc7613714d7d0ae0beb40c00d
Christoffer Dahlen <74780529+ChristofferDahlen@users.noreply.github.com>
2026-02-18 20:14:50 +0100

    Add SettingsView component with tabs and shortcuts
    Introduce a new SettingsView component utilizing PrimeVue's tab and accordion components. It includes a "Main" tab and a "Shortcuts" tab with dynamic content rendered from RollerShortcuts. This provides a structured and interactive UI for managing settings.

---
+++ app/SettingsView.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+import Tab from "primevue/tab";
+import TabList from "primevue/tablist";
+import TabPanels from "primevue/tabpanels";
+import {RollerShortcuts} from "./ts/settings";
+</script>
+
+
+<template>
+  <Tabs value="1" class="overflow-clip">
+
+    <TabList class="tabs">
+      <Tab value="0">Main</Tab>
+      <Tab value="1">Shortcuts</Tab>
+    </TabList>
+    <TabPanels class="overflow-clip">
+      <TabPanel value="0">
+
+        aaa
+      </TabPanel>
+      <TabPanel value="1" class="">
+
+        <Accordion multiple >
+          <AccordionPanel v-for="(shorts, group, i) in RollerShortcuts" :value="i" class="w-72" >
+            <AccordionHeader class="m-2 p-2 w-72">
+              {{group}}
+            </AccordionHeader>
+            <AccordionContent>
+              <div v-for="(a,b) in shorts" class="flex justify-between">
+                <div class="inline-block align-baseline ">{{b}}</div>
+                <Button class="inline-block">{{a}} </Button>
+              </div>
+            </AccordionContent>
+          </AccordionPanel>
+        </Accordion>
+        bbb
+      </TabPanel>
+    </TabPanels>
+  </Tabs>
+</template>
